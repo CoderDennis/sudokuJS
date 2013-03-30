5d61a881e4073eb086eac2659631f927e5217dca
CoderDennis <dodava@gmail.com>
2013-03-30 09:43:01 -0500

    implements manuallyRemove method

--- sudoku/possibleValues.js
+++ sudoku/possibleValues.js
@@ -2,22 +2,35 @@ sudoku.PossibleValues = function () {
 
     var self = this;
 
-    this.values = sudoku.AllNonEmptyValues();
+    this._values = sudoku.AllNonEmptyValues();
     this.manuallyRemoved = [];     // need to make manuallyRemoved a "private" member as shown in knockout js video
 
+    this.values = function() {
+        return self._values;
+    };
+
     this.add = function (value) {
-        if (!_.contains(self.values, value) && !_.contains(self.manuallyRemoved, value))
-            self.values.push(value);
+        if (_.contains(sudoku.AllNonEmptyValues(), value)) {
+            if (!_.contains(self.values, value) && !_.contains(self.manuallyRemoved, value)) {
+                self._values.push(value);
+                self._values.sort();
+            }
+        }
     };
 
     this.remove = function(value) {
-        self.values = _.without(self.values, value);
+        self._values = _.without(self._values, value);
     };
 
     this.manuallyRemove = function(value) {
         self.remove(value);
         if (!_.contains(self.manuallyRemoved, value))
             self.manuallyRemoved.push(value);
+    };
+
+    this.manuallyAdd = function(value) {
+        self.manuallyRemoved = _.without(self.manuallyRemoved, value);
+        self.add(value);
     };
 
 };
--- tests/tests.js
+++ tests/tests.js
@@ -1,12 +1,20 @@
 test("PossibleValues new ContainsValuesOneToNine", function () {
     var testValues = new sudoku.PossibleValues();
-    deepEqual(testValues.values, [1, 2, 3, 4, 5, 6, 7, 8, 9]);
+    deepEqual(testValues.values(), [1, 2, 3, 4, 5, 6, 7, 8, 9]);
 });
 
 test("PossibleValues ManuallyRemovedValue DoesNotGetAdded", function () {
     var testValues = new sudoku.PossibleValues();
     testValues.manuallyRemove(sudoku.values.five);
-    deepEqual(testValues.values, [1, 2, 3, 4, 6, 7, 8, 9], "value five should have been removed.");
+    deepEqual(testValues.values(), [1, 2, 3, 4, 6, 7, 8, 9], "value five should have been removed.");
     testValues.add(sudoku.values.five);
-    deepEqual(testValues.values, [1, 2, 3, 4, 6, 7, 8, 9], "value five should *NOT* have been added.");
+    deepEqual(testValues.values(), [1, 2, 3, 4, 6, 7, 8, 9], "value five should *NOT* have been added.");
+});
+
+test("PossibleValues ManuallyRemovedAndAddedValue DoesGetAdded", function () {
+    var testValues = new sudoku.PossibleValues();
+    testValues.manuallyRemove(sudoku.values.five);
+    deepEqual(testValues.values(), [1, 2, 3, 4, 6, 7, 8, 9], "value five should have been removed.");
+    testValues.manuallyAdd(sudoku.values.five);
+    deepEqual(testValues.values(), [1, 2, 3, 4, 5, 6, 7, 8, 9], "value five should have been added.");
 });
